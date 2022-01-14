043b2382da36bd12670e8d63fa9e6df36e6df866
ChasJC23 <chas.coxell@gmail.com>
2022-01-14 19:22:13 +0000

    added compiler project, nothing special

---
+++ MyLanguage.Compiler/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.CommandLine;
+using System.IO;
+
+namespace MyLanguage.Compiler
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+
+        }
+    }
+}
